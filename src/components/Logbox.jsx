46d4851dfe3da8a941e05961d96eba6769bda4c1
ninecats <delislej@gmail.com>
2022-05-11 14:19:17 -0700

    make logbox scroll, stop mobile view stretching

--- src/components/Logbox.jsx
+++ src/components/Logbox.jsx
@@ -30,7 +30,7 @@ function Logbox(){
     return (
         <div id="output" className="output" style={{width: '50%'}}>
             <div id="content" style={{backgroundColor: "#f0f0f0", borderRadius: "0.75em", display: "block", margin: "0.5em", padding: "0.5em"}}>
-              <div id="status" style={{margin: ".5em 0", fontStyle: "italic"}}>
+              <div id="status" style={{margin: ".5em 0", fontStyle: "italic", height: '100px', overflowY: 'scroll' }}>
                 <pre id="log" style={{margin: ".5em 0", whiteSpace: "pre-wrap"}}></pre>
               </div>
             </div>
