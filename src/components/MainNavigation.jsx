dd1c543a43f7474304391f0180409e1034158600
ninecats <delislej@gmail.com>
2022-05-18 19:52:00 -0700

    rename bind controller, remove old navigation

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,177 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+//drawer elements used
+import Drawer from "@mui/material/Drawer";
+import CloseIcon from "@mui/icons-material/Close";
+import Divider from "@mui/material/Divider";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import FolderIcon from "@mui/icons-material/Folder";
+import ImageIcon from "@mui/icons-material/Image";
+import DescriptionIcon from "@mui/icons-material/Description";
+import { NavLink } from "react-router-dom";
+
+function MainNavigation() {
+
+  /*
+  react useState hook to save the current open/close state of the drawer,
+  normally variables dissapear afte the function was executed
+  */
+  const [open, setState] = useState(false);
+
+  
+  /*
+  function that is being called every time the drawer should open or close,
+  the keys tab and shift are excluded so the user can focus between
+  the elements with the keys
+  */
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    //changes the function state according to the value of open
+    setState(open);
+  };
+
+  return (
+
+    <AppBar position="static">
+      <Container maxWidth="lg" disableGutters="true">
+        <Toolbar>
+
+            <Typography variant="h6" sx={{flexGrow: 1, fontWeight: 700}}>
+              BlueRetro
+            </Typography>
+
+            <Box component="div" sx={{
+              display: {
+                xs: 'none',
+                sm: 'block',
+              }
+            }}>
+              
+            </Box>
+
+            <IconButton 
+              edge="start" 
+              color="inherit" 
+              aria-label="open drawer" 
+              onClick={toggleDrawer(true)}
+              sx={{ 
+                mr: 2,
+                display: {
+                  xs: 'block',
+                  sm: 'none',
+                }
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            {/* The outside of the drawer */}
+            <Drawer
+              //from which side the drawer slides in
+              anchor="right"
+              //if open is true --> drawer is shown
+              open={open}
+              //function that is called when the drawer should close
+              onClose={toggleDrawer(false)}
+              //function that is called when the drawer should open
+              onOpen={toggleDrawer(true)}
+            >
+                {/* The inside of the drawer */}
+                <Box sx={{
+                  p: 2,
+                  height: 1,
+                  backgroundColor: "#dbc8ff",
+                }}>
+
+                  {/* 
+                  when clicking the icon it calls the function toggleDrawer 
+                  and closes the drawer by setting the variable open to false
+                  */}
+                  <IconButton sx={{mb: 2}}>
+                    <CloseIcon onClick={toggleDrawer(false)} />
+                  </IconButton>
+
+                  <Divider sx={{mb: 2}} />
+
+                  <Box sx={{mb: 2}}>
+                  <NavLink to="/advancedconfig">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <ImageIcon sx={{color: "primary.main"}}/>
+                      </ListItemIcon>
+                      <ListItemText primary="Advanced Config" />
+                      
+                    </ListItemButton>
+                    </NavLink>
+
+                    <NavLink to="/presets">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <DescriptionIcon sx={{color: "primary.main"}}/>
+                      </ListItemIcon >
+                      <ListItemText primary="Presets" />
+                    </ListItemButton>
+                    </NavLink>
+
+                    <NavLink to="/n64ctrlpak">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <FolderIcon sx={{color: "primary.main"}} />
+                      </ListItemIcon>
+                      <ListItemText primary="N64 controller pak manager" />
+                    </ListItemButton>
+                    </NavLink>
+
+                    <NavLink to="/presets">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <FolderIcon sx={{color: "primary.main"}} />
+                      </ListItemIcon>
+                      <ListItemText primary="Presets" />
+                    </ListItemButton>
+                    </NavLink>
+
+                    <NavLink to="/presetsmaker">
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <FolderIcon sx={{color: "primary.main"}} />
+                      </ListItemIcon>
+                      <ListItemText primary="Preset Maker" />
+                    </ListItemButton>
+                    </NavLink>
+                  </Box>
+
+                  <Box sx={{
+                    display: "flex", 
+                    justifyContent:"center", 
+                    position: "absolute", 
+                    bottom: "0", 
+                    left: "50%", 
+                    transform: "translate(-50%, 0)"}}
+                  >
+
+                  </Box>
+                </Box>
+              
+            </Drawer>
+           
+
+          </Toolbar>
+      </Container>
+    </AppBar>
+
+  );
+}
+
+export default MainNavigation
--- src/index.js
+++ src/index.js
@@ -6,25 +6,27 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import N64ctrlpak from "./components/N64ctrlpak";
 import Home from "./components/Home";
 import Presets from "./components/Presets";
-import Navigation from "./components/Navigation";
 import Ota from "./components/Ota";
 import Advancedconfig from "./components/Advancedconfig";
-import BindController from "./components/Bindcontroller";
+import Presetsmaker from "./components/Presetsmaker";
+import MainNavigation from "./components/MainNavigation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <div>
   <Router>
-    <Navigation />
+    <MainNavigation/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/n64ctrlpak" element={<N64ctrlpak />} />
       <Route path="/advancedconfig" element={<Advancedconfig />} />
       <Route path="/presets" element={<Presets />} />
-      <Route path="/bindcontroller" element={<BindController />} />
+      <Route path="/presetsmaker" element={<Presetsmaker />} />
       <Route path="/ota" element={<Ota />} />
     </Routes>
   </Router>
+  </div>
 );
 
 serviceWorker.unregister();
