1685cd6f50fe75390d2ae7ff1c9ea9e180bb7ab7
delislej <delislej@gmail.com>
2022-06-01 19:07:52 -0700

    more debugging

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,9 +27,9 @@ function Home() {
   const [globalCfg, setGlobalCfg] = useState(new Uint8Array(4).fill(255));
   const [brSpiffs, setBrSpiffs] = useState("");
   const navigate = useNavigate();
+  var apiver = -1;
 
-  const btConn = () => {
-    let apiver = -1;
+  const btConn = async () => {
     setBluetoothDevice(null);
     ChromeSamples.clearLog();
     ChromeSamples.log("Requesting Bluetooth Device...");
@@ -53,107 +53,15 @@ function Home() {
         //get primary bluetooth service
         return server.getPrimaryService(brUuid[0]);
       })
-      .then((service) => {
+      .then(async (service) => {
         // save service to hook for access from all components
         setBtService(service);
         //access app version characteristic
-        service
-          .getCharacteristic(brUuid[9])
-          .then((chrc) => {
-            ChromeSamples.log("Reading App version...");
-            return chrc.readValue();
-          })
-          .then((value) => {
-            var enc = new TextDecoder("utf-8");
-            ChromeSamples.log("App version: " + enc.decode(value));
-            let version = enc.decode(value).split(" ")[0].split("v")[1];
-            //check if BR version allows for n64 memory pak management, if so save true to hook
-            if (versionCompare("1.6.1", version) <= 0) {
-              setAllowPakManager(true);
-            }
-            //we need to search for _external or _internal then slice the game console
-            if (enc.decode(value).search("internal") === -1) {
-              setBrSpiffs(
-                enc.decode(value).split(" ")[1].split("_external")[0]
-              );
-            } else {
-              setBrSpiffs(
-                enc.decode(value).split(" ")[1].split("_internal")[0]
-              );
-            }
+        await getAppVersion(service);
 
-            if (enc.decode(value).search("n64") !== 1) {
-              setAllowN64(true);
-            }
-            //navigate to home component and hide loading/show navbar
-            navigate("/");
-            setShowLoading(false);
-            setShowNavMenu(true);
-          });
-          ChromeSamples.log("getting api version");
-          service
-          .getCharacteristic(brUuid[6])
-          .then((chrc) => {
-            ChromeSamples.log("Reading Api version...");
-            return chrc.readValue();
-          })
-          .then((value) => {
-            apiver = value.getUint8(0);
-            ChromeSamples.log("Api version: " + apiver);
-          })
-          .catch((error) => {
-            setShowNavMenu(false);
-            navigate("/");
-            setShowLoading(false);
-            throw new Error(error);
-          });
+        await getApiVersion(service);
 
-          ChromeSamples.log("getting global config");
-          service
-          .getCharacteristic(brUuid[1])
-          .then((chrc) => {
-            ChromeSamples.log("Reading Global Config...");
-            return chrc.readValue();
-          })
-          .then((value) => {
-            if (apiver === -1) {
-              setShowNavMenu(false);
-              setBluetoothDevice(null);
-              navigate("/");
-              setShowLoading(false);
-              throw new Error("failed to get API version");
-            }
-            ChromeSamples.log("Global Config size: " + value.byteLength);
-            //get global config size from apiVersion
-            var temp = new Uint8Array(2);
-
-            if (apiver > 0) {
-              temp = new Uint8Array(3);
-            }
-            if (apiver > 1) {
-              temp = new Uint8Array(4);
-            }
-
-            //systemCfg value
-            temp[0] = value.getUint8(0);
-            //multitapCfg value
-            temp[1] = value.getUint8(1);
-            if (apiver > 0) {
-              //document.getElementById("inquiryMode").value = value.getUint8(2);
-              temp[2] = value.getUint8(2);
-            }
-            if (apiver > 1) {
-              //bank select value
-              temp[3] = value.getUint8(3);
-            }
-            setGlobalCfg(temp);
-          })
-          .catch((error) => {
-            ChromeSamples.log("Argh! " + error);
-            setShowNavMenu(false);
-            navigate("/");
-            setShowLoading(false);
-          });
+        await getGlobalCfg(service);
       })
       //on error, print to logbox and force navigate back to home, hide loading for when error occures on connect
       .catch((error) => {
@@ -162,6 +70,114 @@ function Home() {
         navigate("/");
         setShowLoading(false);
       });
+  };
+
+  const getAppVersion = async (service) => {
+    return new Promise((resolve) => {
+      service
+        .getCharacteristic(brUuid[9])
+        .then((chrc) => {
+          ChromeSamples.log("Reading App version...");
+          return chrc.readValue();
+        })
+        .then((value) => {
+          var enc = new TextDecoder("utf-8");
+          ChromeSamples.log("App version: " + enc.decode(value));
+          let version = enc.decode(value).split(" ")[0].split("v")[1];
+          //check if BR version allows for n64 memory pak management, if so save true to hook
+          if (versionCompare("1.6.1", version) <= 0) {
+            setAllowPakManager(true);
+          }
+          //we need to search for _external or _internal then slice the game console
+          if (enc.decode(value).search("internal") === -1) {
+            setBrSpiffs(enc.decode(value).split(" ")[1].split("_external")[0]);
+          } else {
+            setBrSpiffs(enc.decode(value).split(" ")[1].split("_internal")[0]);
+          }
+
+          if (enc.decode(value).search("n64") !== 1) {
+            setAllowN64(true);
+          }
+          //navigate to home component and hide loading/show navbar
+          navigate("/");
+          setShowLoading(false);
+          setShowNavMenu(true);
+        });
+      resolve();
+    });
+  };
+
+  const getApiVersion = async (service) => {
+    return new Promise((resolve) => {
+      service
+        .getCharacteristic(brUuid[6])
+        .then((chrc) => {
+          ChromeSamples.log("Reading Api version...");
+          return chrc.readValue();
+        })
+        .then((value) => {
+          apiver = value.getUint8(0);
+          ChromeSamples.log("Api version: " + apiver);
+        })
+        .catch((error) => {
+          setShowNavMenu(false);
+          navigate("/");
+          setShowLoading(false);
+          throw new Error(error);
+        });
+      resolve();
+    });
+  };
+
+  const getGlobalCfg = async (service) => {
+    return new Promise((resolve) => {
+      service
+        .getCharacteristic(brUuid[1])
+        .then((chrc) => {
+          ChromeSamples.log("Reading Global Config...");
+          return chrc.readValue();
+        })
+        .then((value) => {
+          if (apiver === -1) {
+            setShowNavMenu(false);
+            setBluetoothDevice(null);
+            navigate("/");
+            setShowLoading(false);
+            throw new Error("failed to get API version");
+          }
+          ChromeSamples.log("Global Config size: " + value.byteLength);
+          //get global config size from apiVersion
+          var temp = new Uint8Array(2);
+
+          if (apiver > 0) {
+            temp = new Uint8Array(3);
+          }
+          if (apiver > 1) {
+            temp = new Uint8Array(4);
+          }
+
+          //systemCfg value
+          temp[0] = value.getUint8(0);
+          //multitapCfg value
+          temp[1] = value.getUint8(1);
+          if (apiver > 0) {
+            //document.getElementById("inquiryMode").value = value.getUint8(2);
+            temp[2] = value.getUint8(2);
+          }
+          if (apiver > 1) {
+            //bank select value
+            temp[3] = value.getUint8(3);
+          }
+          setGlobalCfg(temp);
+          resolve();
+        })
+        .catch((error) => {
+          ChromeSamples.log("Argh! " + error);
+          setShowNavMenu(false);
+          navigate("/");
+          setShowLoading(false);
+        });
+    });
   };
 
   const onDisconnected = () => {
