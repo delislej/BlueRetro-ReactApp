2a12d29954cea2a39508f5ed9b3a696eccd92e60
delislej <delislej@gmail.com>
2022-06-08 13:09:11 -0700

    remove typography from home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
 import N64Configuration from "./N64Configuration";
