a70409d6c1a236f40ae8fb1f7cc00d2cfdc1717b
delislej <delislej@gmail.com>
2022-06-07 22:42:26 -0700

    finished presets maker work

--- src/components/Presetsmaker.jsx
+++ src/components/Presetsmaker.jsx
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
-//import { useGamepads } from 'awesome-react-gamepads';
-//var bluetoothDevice;
 import { btnList, btn } from "../utils/constants";
 import savePresetInput from "../utils/savePresetInput";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Paper } from "@mui/material";
 import { gameConsoleControllers } from "../utils/constants";
 import { controllers } from "../utils/constants";
@@ -16,6 +14,7 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AccordionActions from "@mui/material/AccordionActions";
 import Stack from "@mui/material/Stack";
+import { useFilePicker } from "use-file-picker";
 
 const Presetsmaker = (props) => {
   const myrange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -31,18 +30,23 @@ const Presetsmaker = (props) => {
   const [inputOptions, setInputOptions] = useState({ value: 0, label: 0 });
   const [expanded, setExpanded] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [openFileSelector, { filesContent, clear }] = useFilePicker({
+    accept: ".json",
+    multiple: false,
+    readAs: "Text",
+  });
   var filterArr = [];
 
-  //const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  /*useEffect(() => {
+  useEffect(() => {
     if (props.btDevice === null) {
       navigate("/");
     }
     if (props.globalCfg[0] === 255) {
       navigate("/");
     }
-  }, [props.btDevice, props.globalCfg, navigate]);*/
+  }, [props.btDevice, props.globalCfg, navigate]);
 
   useEffect(() => {
     var cons = controllers.map(function (value, index) {
@@ -112,6 +116,24 @@ const Presetsmaker = (props) => {
     setConsoleController(temp);
   };
 
+  const downloadJson = (obj) => {
+    const element = document.createElement("a");
+    let temp = new Blob([JSON.stringify(obj)], { type: "application/json" });
+    element.href = URL.createObjectURL(temp);
+    element.download = "userFile.json";
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  const makeJson = (x, y, z) => {
+    let temp = {
+      gameConsoleController: x,
+      bluetoothController: y,
+      bindings: z,
+    };
+    return temp;
+  };
+
   const createSelects = (gcCon, con) => {
     if (gcCon === -1 || con === -1) {
       return null;
@@ -150,10 +172,9 @@ const Presetsmaker = (props) => {
   };
 
   const handleChange = (panel) => (event, isExpanded) => {
-    if(panel === "panel1"){
+    if (panel === "panel1") {
       setIsDisabled(true);
     }
-    console.log("handle expand");
     setExpanded(isExpanded ? panel : false);
   };
 
@@ -203,27 +224,72 @@ const Presetsmaker = (props) => {
         }
       }
     }
+    console.log(tempMap);
+    json.map = tempMap;
+    savePresetInput(json, props.btService, input);
+  };
+
+  const saveUploadedBindings = (file) => {
+    var buttonKeys = Object.keys(btn);
+    let json = {
+      name: "preset maker json",
+      desc: "used as a shell for making presets",
+      console: "NA",
+      map: [],
+    };
+    let tempMap = [];
+
+    for (let i = 0; i < file.bindings.length; i++) {
+      if (file.bindings[i].bindings.length < 1) {
+        let arr = [];
+        arr.push(buttonKeys[file.bindings[i].value]);
+        arr.push(buttonKeys[file.bindings[i].value]);
+        arr.push(0);
+        arr.push(100);
+        arr.push(50);
+        arr.push(135);
+        arr.push(0);
+        arr.push(0);
+        arr.push(0);
+        tempMap.push(arr);
+      } else {
+        for (let j = 0; j < file.bindings[i].bindings.length; j++) {
+          let arr = [];
+          arr.push(buttonKeys[file.bindings[i].bindings[j].value]);
+          arr.push(buttonKeys[file.bindings[i].value]);
+          arr.push(0);
+          arr.push(100);
+          arr.push(50);
+          arr.push(135);
+          arr.push(0);
+          arr.push(0);
+          arr.push(0);
+          tempMap.push(arr);
+        }
+      }
+    }
     json.map = tempMap;
     savePresetInput(json, props.btService, input);
   };
 
   return (
     <Paper sx={{ p: 2, marginBottom: "25px", width: { xs: "90%", lg: "66%" } }}>
-      <Accordion elevation={5}
-      expanded={expanded === "panel1"}
-      onChange={handleChange("panel1")}
+      <Accordion
+        elevation={5}
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
       >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>select systems</Typography>
+          <Typography>Controller Selection</Typography>
         </AccordionSummary>
         <AccordionDetails sx={{ alignItems: "center" }}>
           <Stack spacing={3}>
             <Typography>Bindings</Typography>
-            
+
             <Select
               defaultValue={""}
               isSearchable={false}
@@ -250,34 +316,44 @@ const Presetsmaker = (props) => {
         </AccordionDetails>
         <AccordionActions>
           <Button
+            variant="outlined"
             onClick={() => {
-              createSelects(selectedGameConsoleController, selectedController);
-              setIsDisabled(false);
+              if (
+                selectedController !== -1 &&
+                selectedGameConsoleController !== -1
+              ) {
+                createSelects(
+                  selectedGameConsoleController,
+                  selectedController
+                );
+                setIsDisabled(false);
+                setExpanded("panel2");
+              }
             }}
           >
             Start
           </Button>
         </AccordionActions>
       </Accordion>
-      <Accordion elevation={5}
-      expanded={expanded === "panel2"}
-      disabled={isDisabled}
-      onChange={handleChange("panel2")}
+      <Accordion
+        elevation={5}
+        expanded={expanded === "panel2"}
+        disabled={isDisabled}
+        onChange={handleChange("panel2")}
       >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>select systems</Typography>
+          <Typography>Bindings</Typography>
         </AccordionSummary>
         <AccordionDetails sx={{ alignItems: "center" }}>
-          <Stack spacing={3} sx={{ height: {xs:"300px", lg:"700"}, overflowY: "auto" }}>
-            {selects}
-          </Stack>
-        </AccordionDetails>
-        <AccordionActions>
-        <Select
+          <Stack
+            spacing={3}
+            sx={{ height: { xs: "300px", lg: "700" }, overflowY: "auto" }}
+          >
+            <Select
               defaultValue={""}
               isSearchable={false}
               name="input"
@@ -288,14 +364,86 @@ const Presetsmaker = (props) => {
               className="basic-multi-select"
               classNamePrefix="select"
             />
-          <Button
-            onClick={() => {
-              saveBindings();
-            }}
-          >
-            Save Bindings
-          </Button>
+            {selects}
+          </Stack>
+        </AccordionDetails>
+        <AccordionActions>
+          <Stack direction={"row"} spacing={1}>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                saveBindings();
+              }}
+            >
+              Save Bindings
+            </Button>
+
+            <Button
+              variant="outlined"
+              onClick={() => {
+                downloadJson(
+                  makeJson(
+                    selectedGameConsoleController,
+                    selectedController,
+                    consoleController
+                  )
+                );
+              }}
+            >
+              Save to File
+            </Button>
+          </Stack>
         </AccordionActions>
+      </Accordion>
+
+      <Accordion
+        elevation={5}
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Upload Preset</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ alignItems: "center" }}>
+          <Stack spacing={3}>
+            <Select
+              defaultValue={""}
+              isSearchable={false}
+              name="input"
+              options={inputOptions}
+              onChange={(x) => {
+                handleInputChange(x);
+              }}
+              className="basic-multi-select"
+              classNamePrefix="select"
+            />
+            <Button
+              variant="outlined"
+              id="fileSelector"
+              onClick={() => {
+                openFileSelector();
+              }}
+            >
+              {filesContent.length > 0 ? filesContent[0].name : "Select .json"}
+            </Button>
+            {filesContent.length > 0 ? (
+              <Button
+                variant="outlined"
+                id="btnPakWrite"
+                onClick={() => {
+                  saveUploadedBindings(JSON.parse(filesContent[0].content));
+                  clear();
+                }}
+              >
+                Write
+              </Button>
+            ) : null}
+          </Stack>
+        </AccordionDetails>
       </Accordion>
     </Paper>
   );
