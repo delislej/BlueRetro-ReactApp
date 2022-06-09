42bc3d313f7a69c268239d406db816f31eeca147
delislej <delislej@gmail.com>
2022-06-09 12:18:24 -0700

    removed n64 json, made react router not change url for netlify support.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import Home from "./components/Home";
-import { BrowserRouter } from "react-router-dom";
 import Paper from "@mui/material/Paper"
+import { MemoryRouter } from "react-router-dom"
+
 
 ReactDOM.render(
     <Paper square className="index paper" sx={{height: "100vh", display: "flex", flexDirection:"column", backgroundColor: "#a6adff", overflowY: "auto"}}>
-<BrowserRouter>
+<MemoryRouter>
 <Home />
-</BrowserRouter>
+</MemoryRouter>
 </Paper>
 , document.getElementById("root"));
 serviceWorker.unregister();
